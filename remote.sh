f8f1865032fd3d228f13d4e3dd4eb82a38fddd30
Sparky4567 <andrius.pratusis1993@gmail.com>
2022-07-15 15:17:17 +0300

    remote.sh update

--- remote.sh
+++ remote.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 CURRENTDATE=`date +"%Y-%m-%d %T"`
-git add . -f
+git add themelist -f
+git add ideas -f
+git add journal -f
+git add kanban -f
 message="Obsidian update: "$CURRENTDATE
 git commit -m "$message"
 git push
