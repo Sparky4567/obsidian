26921daf4136197de2c8bc6509286ea5e514ecb1
Sparky4567 <andrius.pratusis1993@gmail.com>
2022-07-16 17:05:36 +0300

    remote

--- remote.sh
+++ remote.sh
@@ -5,7 +5,7 @@ git add * content/posts/journal -f
 git add * content/posts/kanban -f
 git add * content/posts/templates -f
 git add * content/posts/themelist -f
-
+git add * public -f
 message="Obsidian update: "$CURRENTDATE
 git commit -m "$message"
 git push
