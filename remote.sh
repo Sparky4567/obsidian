ad38aeecac3b04d1add0eb4474267ba7326cfa6a
Andrius Pratusis <andrius.pratusis1993@gmail.com>
2022-07-17 11:48:26 +0300

    remote update

--- remote.sh
+++ remote.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 CURRENTDATE=`date +"%Y-%m-%d %T"`
-git add themes -f
-git add content/posts/ideas -f
-git add content/posts/journal -f
-git add content/posts/kanban -f
-git add content/posts/templates -f
-git add content/posts/themelist -f
+git add themes/* -f
+git add content/* -f
 git add public -f
 message="Obsidian update: "$CURRENTDATE
 git commit -m "$message"
